37382cf1a617d16b487d6bcf983a89bea5d6a454
sorainu <inukun.net@gmail.com>
2018-12-09 23:07:17 +0900

    てすと２

--- public_html/index.php
+++ public_html/index.php
@@ -22,7 +22,7 @@
 <!DOCTYPE HTML>
 <html lang="ja">
 <head>
-    <title>Todo List</title>
+    <title>Todo List 修正</title>
     <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
     <link rel="stylesheet" href="//cdn.rawgit.com/necolas/normalize.css/master/normalize.css">
     <link rel="stylesheet" href="//cdn.rawgit.com/milligram/milligram/master/dist/milligram.min.css">
